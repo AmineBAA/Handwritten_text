47cefebba3faac797fc68901c5b315d35df08dd1
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-19 18:35:22 +0100

    Update handwritten_text.py

--- handwritten_text.py
+++ handwritten_text.py
@@ -4,7 +4,7 @@ import streamlit as st
 import io
 
 # Set the Tesseract executable path
-pytesseract.pytesseract.tesseract_cmd = r'https://drive.google.com/file/d/1pUXXP80qdlCXnualSIb6XdFovYvM29eO/view?usp=sharing'  # Update this path
+pytesseract.pytesseract.tesseract_cmd = r'C:/Users/AMINE/OneDrive/tessdata'  # Update this path
 
 # Your Streamlit app code
 st.title("Handwritten Text Recognition")
